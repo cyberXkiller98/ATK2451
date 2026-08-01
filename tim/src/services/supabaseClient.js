c3a7e230061f92d90869c50b4903d1d93c7d4647
tima0922 <timur20060922@gmail.com>
2026-08-02 02:14:05 +0500

    Update supabaseClient.js

--- tim/src/services/supabaseClient.js
+++ tim/src/services/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Пропиши свои реальные данные прямо здесь
 const supabaseUrl = 'https://tbypvcdhypdtyyqzxkuk.supabase.co'
-const supabaseAnonKey =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRieXB2Y2RoeXBkdHl5cXp4a3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1OTQ0ODgsImV4cCI6MjEwMTE3MDQ4OH0.RwsaMHqkMKbOt05RvdqssLkU3mmmZxdqk7LQWWythsA
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRieXB2Y2RoeXBkdHl5cXp4a3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1OTQ0ODgsImV4cCI6MjEwMTE3MDQ4OH0.RwsaMHqkMKbOt05RvdqssLkU3mmmZxdqk7LQWWythsA
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+)
