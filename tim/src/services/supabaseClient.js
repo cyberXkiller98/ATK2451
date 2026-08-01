4e1666f600955b89dd4b008432477a78c12204ac
tima0922 <timur20060922@gmail.com>
2026-08-02 02:14:41 +0500

    Fix supabaseAnonKey formatting in supabaseClient.js

--- tim/src/services/supabaseClient.js
+++ tim/src/services/supabaseClient.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://tbypvcdhypdtyyqzxkuk.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRieXB2Y2RoeXBkdHl5cXp4a3VrIiwicm9sZSI6ImFub24iLCJpYX...' // <-- Обязательно в кавычках!
 
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRieXB2Y2RoeXBkdHl5cXp4a3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1OTQ0ODgsImV4cCI6MjEwMTE3MDQ4OH0.RwsaMHqkMKbOt05RvdqssLkU3mmmZxdqk7LQWWythsA
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
