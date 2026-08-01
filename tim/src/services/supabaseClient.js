2a3b1a29783f6e869f389dd320bc8bde4c24c700
tima0922 <timur20060922@gmail.com>
2026-08-02 02:20:23 +0500

    Reformat supabaseClient.js with consistent imports

--- tim/src/services/supabaseClient.js
+++ tim/src/services/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://tbypvcdhypdtyyqzxkuk.supabase.co'
 // ⚠️ ОБЯЗАТЕЛЬНО ДОЛЖНЫ БЫТЬ КАВЫЧКИ С ОБЕИХ СТОРОН КЛЮЧА:
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRieXB2Y2RoeXBkdHl5cXp4a3VrIiwicm9sZSI6ImFub24iLCJpYX...' 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRieXB2Y2RoeXBkdHl5cXp4a3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1OTQ0ODgsImV4cCI6MjEwMTE3MDQ4OH0.RwsaMHqkMKbOt05RvdqssLkU3mmmZxdqk7LQWWythsA' 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
