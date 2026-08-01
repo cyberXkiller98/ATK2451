e2bac0edcb42144e71622ef5144b320852a670da
tima0922 <timur20060922@gmail.com>
2026-08-02 01:59:12 +0500

    Update supabaseClient.js

--- tim/src/services/supabaseClient.js
+++ tim/src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
+// Пропиши свои реальные данные прямо здесь
 const supabaseUrl = 'https://tbypvcdhypdtyyqzxkuk.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRieXB2Y2RoeXBkdHl5cXp4a3VrIiwicm9sZSI6ImFub24iLCJpYX...' // вставь свой ключ целиком
-
+const supabaseAnonKey =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRieXB2Y2RoeXBkdHl5cXp4a3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1OTQ0ODgsImV4cCI6MjEwMTE3MDQ4OH0.RwsaMHqkMKbOt05RvdqssLkU3mmmZxdqk7LQWWythsA
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
